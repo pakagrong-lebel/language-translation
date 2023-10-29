d4e1a7b1538ad7c987bf93a85cafc82d447c55cc
pakagrong-lebel <92211586+pakagrong-lebel@users.noreply.github.com>
2023-10-29 16:11:51 +1100

    Add files via upload

---
+++ translation.py
@@ -0,0 +1,3 @@
+# Python script for language translation using NLP libraries
+# Your code here to connect to a translation API (e.g., Google Translate, Microsoft Translator)
+# Your code here to translate text between different languages
